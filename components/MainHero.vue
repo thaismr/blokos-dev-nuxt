ca70eca5d9978faea508296e0bbaad8eebdaf161
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2022-08-24 19:23:40 -0300

    feat: initial commit

---
+++ components/MainHero.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="hero-container">
+    <h2>Welcome!</h2>
+    <p>Here you can keep track of some of my main projects and their status.</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <MainHero />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
